68453cc69eb448ac88b6248d6d4a532dd4f62c94
Keren <igiranezakkeren16@gmail.com>
2025-03-20 11:01:54 -0700

    Fixed ESLint errors and updated dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
 // import Image from "next/image";
 
-// import DisabilitiesSelection from "./components/DisabilitiesSelection";
-// import SelectionPage from "./components/SelectionPage";
+import DisabilitiesSelection from "./components/DisabilitiesSelection";
+import SelectionPage from "./components/SelectionPage";
 
 import CollectUserInfo from "./components/CollectUserInfo";
 import UserInfoForm from "./components/UserInfoForm";
 import Landing from "./components/Landing";
-// import ChatBot from "./components/ChatBot";
+import ChatBot from "./components/ChatBot";
 
 
 export default function Home() {
   return (
     <div>
-      {/* <UserInfoForm/> */}
+      <UserInfoForm/>
 
-      {/* <CollectUserInfo/> */}
+      <CollectUserInfo/>
      
-     {/* <SelectionPage/> */}
-     {/* <DisabilitiesSelection/> */}
+     <SelectionPage/>
+     <DisabilitiesSelection/>
 
      <Landing/>
-     {/* <ChatBot/> */}
+     <ChatBot/>
      
     </div>
   );
